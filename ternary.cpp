8fd17e28d9eaf112b1e24572c90145ac4a3bbdf9
abnsl0014 <abnsl0014@gmail.com>
2019-05-28 00:09:56 +0530

    Please enter the commit message for your chagges. Lines starting d:   ternary.cpp

--- ternary.cpp
+++ ternary.cpp
@@ -14,9 +14,10 @@ int countBits(int n){
 int main()
 {
   int n=20;
+ 
   //n%7==0? "yes":"no
   cout<<countBits(n);
-
+   //this is the final vanishing part , we are going to include in master branch
   ///left shift , right shift
   //n&1 //gives last digit
    (n&1)? cout<<"yes":cout<<"no";
