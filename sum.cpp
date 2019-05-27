55f08005d65c8c356d9cc7d6b98b369f085ce8da
abnsl0014 <abnsl0014@gmail.com>
2019-05-27 20:50:04 +0530

    yes

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int b=0,n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int a;
+        cin>>a;
+        if(a>b)
+        {
+            b=a;
+        }
+        //b+=a;
+    }
+    cout<<b<<endl;
+}
