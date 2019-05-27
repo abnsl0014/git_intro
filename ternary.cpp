0d9e989bd4e5b91e9a0b7ac65c18c94a65c88504
abnsl0014 <abnsl0014@gmail.com>
2019-05-27 21:00:21 +0530

    adding

---
+++ ternary.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int countBits(int n){
+      //set bits count
+      int cnt=0;
+      while(n>0)
+      {
+          cnt+=(n&1);
+          n=n>>1;
+      }
+      return cnt;
+  }
+int main()
+{
+  int n=20;
+  //n%7==0? "yes":"no
+  cout<<countBits(n);
+
+  ///left shift , right shift
+  //n&1 //gives last digit
+   (n&1)? cout<<"yes":cout<<"no";
+}
